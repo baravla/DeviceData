8870c26428d912a17df218de3cda726a04e658de
vibara <vladimir.m.baranov@gmail.com>
2026-02-27 22:08:00 -0500

    Add project files.

---
+++ DeviceDataModels/DevicePacket.cs
@@ -0,0 +1,8 @@
+﻿namespace DeviceDataModels {
+    public class DevicePacket {
+        public string Source { get; set; } = null!;
+        public DateTime Timestamp { get; set; }
+        public string PatientId { get; set; } = null!;
+        public List<DeviceParameter> Parameters { get; set; } = new();
+    }
+}
---
+++ DeviceDataModels/DeviceParameter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeviceDataModels {
+    public class DeviceParameter {
+        public string Name { get; set; } = null!;
+        public double Value { get; set; }
+    }
+}
