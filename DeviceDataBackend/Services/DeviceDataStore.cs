a6391d717994aeaf53a3a345908cfc70ae0bc0b8
vibara <vladimir.m.baranov@gmail.com>
2026-02-28 19:09:09 -0500

    DeviceDataStore class

---
+++ DeviceDataBackend/Services/DeviceDataStore.cs
@@ -0,0 +1,27 @@
+﻿using DeviceDataModels;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace DeviceDataBackend.Services {
+    public class DeviceDataStore : IDeviceDataStore {
+        MemoryCache _cache = new MemoryCache(new MemoryCacheOptions());
+
+        public void AddDevicePacket(DevicePacket packet) {
+            var key = $"{packet.Source}_{packet.Timestamp:O}";
+            _cache.Set(key, packet);
+        }
+        
+        public IEnumerable<DevicePacket> GetDevicePackets(Func<DevicePacket, bool> filter) {
+            var results = new List<DevicePacket>();
+            foreach (var key in _cache.Keys) {
+                if (_cache.TryGetValue(key, out var value) &&
+                    value is DevicePacket packet &&
+                    filter(packet)) 
+                {
+                    results.Add(packet);
+                }
+            }
+
+            return results;
+        }
+    }
+}
---
+++ DeviceDataBackend/Services/IDeviceDataStore.cs
@@ -0,0 +1,8 @@
+﻿using DeviceDataModels;
+
+namespace DeviceDataBackend.Services {
+    public interface IDeviceDataStore {
+        void AddDevicePacket(DevicePacket packet);
+        IEnumerable<DevicePacket> GetDevicePackets(Func<DevicePacket, bool> filter);
+    }
+}
